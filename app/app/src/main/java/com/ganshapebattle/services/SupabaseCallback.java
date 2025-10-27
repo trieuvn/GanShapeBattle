e39a37e06a40716ddc7b3b93c6daa97a44899795
trieuvn <trieu123ok@gmail.com>
2025-10-27 22:14:53 +0700

    remove duplicated app

--- app/app/src/main/java/com/ganshapebattle/services/SupabaseCallback.java
+++
@@ -1,8 +0,0 @@
-package com.ganshapebattle.services;
-
-// Sử dụng Generics <T> để callback có thể trả về bất kỳ kiểu đối tượng nào
-// (ví dụ: List<User>, Gallery, String, ...)
-public interface SupabaseCallback<T> {
-    void onSuccess(T result);
-    void onFailure(Exception e);
-}
